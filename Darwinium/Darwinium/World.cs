9410bd4d2605066fb0c652e0ea4da95d60c6c44e
Grant Hixson <ghixsonwcl@gmail.com>
2015-06-29 16:01:50 -0500

    Added World.
    Added World class.

---
+++ Darwinium/Darwinium/World.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Darwinium
+{
+    class World
+    {
+        public static World CurrentWorld { get; private set; }
+
+        // World Values
+        public int Oxygen;
+        public int Nitrogen;
+        public int Carbon;
+        public int Water;
+
+        public World(int O2, int N, int C, int H2O)
+        {
+            Oxygen = O2;
+            Nitrogen = N;
+            Carbon = C;
+            Water = H2O;
+
+            World.CurrentWorld = this;
+        }
+    }
+}
